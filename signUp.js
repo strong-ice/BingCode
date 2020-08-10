5911f60cfe96938eab5a87c072faae649047e013
strong-ice <binghwa0610@gmail.com>
2020-08-10 21:16:19 +0900

    validatin

--- signUp.js
+++ signUp.js
@@ -6,9 +6,14 @@ let doc = document;
 let func = function() {
     // 1. 정보 취합
     let userEmail = doc.getElementById('userEmail');
+    let cerNum = doc.getElementById('cerNum');
     let userPass = doc.getElementById('userPass');
     let userConfirm = doc.getElementById('userConfirm');
 
+    // 이메일 정규화 공식([]안의 값만 쓰겠다)
+    let regul1 = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/;
+    // 비밀번호 정규화 공식
+    let regul2 = /^[a-zA-Z0-9]{4,12}$/;
 
         // console.log(userEmail.value);
         // console.log(userPass.value);
@@ -23,11 +28,35 @@ let func = function() {
         userEmail.focus();
         return;
     }
+
+    // 이메일이 형식에 맞지 않는 경우
+    if (!regul1.test(userEmail.value)){
+        alert('이메일 형식이 맞지 않습니다.');
+        userEmail.focus();
+        return;
+    }
+
+    if (!cerNum.value) {
+        alert('인증번호를 입력하세요.');
+        cerNum.focus();
+        return;
+    }
+
+    // 비밀번호를 입력하지 않은 경우
     if (!userPass.value) {
         alert('비밀번호를 입력하세요.');
         userPass.focus();
         return;
     }
+
+    // 비밀번호가 형식에 맞지 않는 경우
+    if (!regul2.test(userPass.value)){
+        alert('비밀번호 형식이 맞지 않습니다.');
+        userPass.focus();
+        return;
+    }
+
+    // 비밀번호 확인을 입력하지 않은 경우
     if (!userConfirm.value) {
         alert('비밀번호 확인을 입력하세요.');
         userConfirm.focus();
